b8474468427876fa3b4ca67df2bb2bb8115576a0
vipulrawat <vipulrawat.imi@live.in>
2020-06-14 23:40:27 +0530

    Updates

---
+++ precache-manifest.aabfd61aa68fd6cb2caf30f0b2e1134c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c61483a24e0acce800309090c140f7e",
+    "url": "/wsui/index.html"
+  },
+  {
+    "revision": "0f000afb67b29a94cd5f",
+    "url": "/wsui/static/css/main.6c6ca8a9.chunk.css"
+  },
+  {
+    "revision": "6916039da902f908d30d",
+    "url": "/wsui/static/js/2.fec9d45e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wsui/static/js/2.fec9d45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f000afb67b29a94cd5f",
+    "url": "/wsui/static/js/main.c914c20e.chunk.js"
+  },
+  {
+    "revision": "b39f5e23e41623f5f001",
+    "url": "/wsui/static/js/runtime-main.d76dfec3.js"
+  }
+]);
